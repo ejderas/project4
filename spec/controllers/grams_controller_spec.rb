1dce75e5775b63af6c44eb2dc5c40cd18ad52d24
Erika <ejderas2@gmail.com>
2018-10-14 09:29:15 +0000

    Adding Factory Bot so as not to use Fixtrures

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -16,11 +16,7 @@ RSpec.describe GramsController, type: :controller do
     
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -37,11 +33,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
       
 
@@ -55,11 +47,7 @@ RSpec.describe GramsController, type: :controller do
 
 
       it "should properly deal with validation errors" do
-        user = User.create(
-          email:                 'fakeuser@gmail.com',
-          password:              'secretPassword',
-          password_confirmation: 'secretPassword'
-        )
+        user = FactoryBot.create(:user)
         sign_in user 
 
         gram_count = Gram.count 
